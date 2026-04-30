8d08791a8a51271523de13c602b5e2c8c93f3aa7
ruzanovsasa367-beep <ruzanovsasa367@gmail.com>
2026-04-30 03:11:26 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
+-- 1. Загружаем библиотеку интерфейса
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+-- 2. Показываем уведомление о начале загрузки
+Rayfield:Notify({
+   Title = "Загрузка...",
+   Content = "Пожалуйста, подождите, основной скрипт загружается",
+   Duration = 3,
+   Image = 4483362458,
+})
+
+-- 3. Загружаем и запускаем ваш основной скрипт
+-- Замените 'ССЫЛКА_НА_ВАШ_СКРИПТ' на прямую ссылку (raw)
+task.spawn(function()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet('https://githubusercontent.com'))()
+    end)
+
+    if not success then
+        warn("Ошибка загрузки основного скрипта: " .. err)
+    end
+end)
 
